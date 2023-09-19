100dc74483b0e5c1d3b35d25caa988ff1a2716d2
BwithE <144924113+BwithE@users.noreply.github.com>
2023-09-19 10:50:00 -0400

    boot-tools.sh

--- boot-tools.sh
+++ boot-tools.sh
@@ -7,7 +7,7 @@ clear
 # Checks to verify that the script is running as root
 if [[ $EUID -ne 0 ]]; then
    echo "THIS SCRIPT NEEDS TO BE RUN AS ROOT."
-   echo "EX: sudo ./linux-tools.sh"
+   echo "EX: sudo ./boot-tools.sh"
    exit 1
 fi
 
