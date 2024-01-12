4b096cbb0a337a9562e46499ed7e3bef0c3ccade
BwithE <144924113+BwithE@users.noreply.github.com>
2024-01-12 16:56:32 -0500

    Update boot-tools.sh

--- boot-tools.sh
+++ boot-tools.sh
@@ -22,163 +22,84 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# this script checks for the operating system
-# this portion is for ubuntu
-if [[ "$(cat /etc/os-release | grep ID=ubuntu)" == 'ID=ubuntu' ]]; then
-  # do stuff for Ubuntu
-  echo "This is Ubuntu!"
-    echo "INSTALLING UPDATES"
-    sudo apt update -y #&& sudo apt upgrade -y
+# function to install packages
+install_packages() {
+    if command -v apt &> /dev/null; then
+        sudo apt install -y "${@}"
+    elif command -v yum &> /dev/null; then
+        sudo yum install -y "${@}"
+    else
+        echo "unsupported package manager. exiting."
+        exit 1
+    fi
     clear
-    echo "INSTALLING TERMINATOR"
-    sudo apt install terminator -y
-    clear
-    echo "INSTALLING NET TOOLS"
-    sudo apt install net-tools -y
-    clear
-    echo "INSTALLING NMAP"
-    sudo apt install nmap -y
-    clear
-    echo "INSTALLING ARP-SCAN"
-    sudo apt install arp-scan -y
-    clear
-    echo "INSTALLING AIRCRACK"
-    sudo apt-get install aircrack-ng -y
-    clear
-    echo "INSTALLING WIRESHARK"
-    sudo apt-get install wireshark -y
-    clear
-    echo "INSTALLING T-SHARK"
-    sudo apt install tshark -y
-    clear
-    echo "INSTALLING TREE"
-    sudo apt install tree -y
-    clear
-    echo "INSTALLING ETTERCAP"
-    sudo apt install ettercap-text-only -y
-    clear
-    echo "INSTALLING METASPLOIT"
-    sudo apt install gpgv2 autoconf bison build-essential postgresql libaprutil1 libgmp3-dev libpcap-dev openssl libpq-dev libreadline6-dev libsqlite3-dev libssl-dev locate libsvn1 libtool libxml2 libxml2-dev libxslt-dev wget libyaml-dev ncurses-dev  postgresql-contrib xsel zlib1g zlib1g-dev -y
-    sudo rm -r msfinstall
-    clear
-    curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
-    sudo chmod 755 msfinstall
-    sudo ./msfinstall
-    sudo rm -rf msfinstall
-    clear
-    echo "INSTALLING QRENCODE"
-    sudo apt install qrencode -y
-    clear
-    echo "INSTALLING SSH AND FTP"
-    sudo apt install ssh -y && sudo apt install vsftpd -y && sudo apt install ftp -y
-    sudo systemctl stop ssh
-    sudo systemctl disable ssh
-    sudo systemctl stop vsftpd
-    sudo systemctl disable vsftpd
-    clear
-    echo "INSTALLING STEGHIDE"
-    sudo apt install steghide -y
-    clear
-# takes away case in-sensitive on ubuntu
-    echo "CHANGING CASE SENSITIVITY"
-    sudo echo 'set completion-ignore-case On' >> /etc/inputrc
-    sudo echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
-    clear
+}
 
-# this portion is for rasbpian
-elif [[ "$(cat /etc/os-release | grep ID=raspbian)" == 'ID=raspbian' ]]; then
-  # do stuff for Raspbian
-  echo "This is Raspbian!"
-    echo "INSTALLING UPDATES"
-    sudo apt update -y #&& sudo apt upgrade -y
-    clear
-    echo "INSTALLING TERMINATOR"
-    sudo apt install terminator -y
-    clear
-    echo "INSTALLING NET TOOLS"
-    sudo apt install net-tools -y
-    clear
-    echo "INSTALLING NMAP"
-    sudo apt install nmap -y
-    clear
-    echo "INSTALLING ARP-SCAN"
-    sudo apt install arp-scan -y
-    clear
-    echo "INSTALLING ETTERCAP"
-    sudo apt install ettercap-text-only -y
-    clear
-    echo "INSTALLING AIRCRACK"
-    sudo apt-get install aircrack-ng -y
-    clear
-    echo "INSTALLING WIRESHARK"
-    sudo apt-get install wireshark -y
-    clear
-    echo "INSTALLING T-SHARK"
-    sudo apt install tshark -y
-    clear
-    echo "INSTALLING TREE"
-    sudo apt install tree -y
-    clear
-    echo "INSTALLING METASPLOIT"
-    sudo apt install gpgv2 autoconf bison build-essential postgresql libaprutil1 libgmp3-dev libpcap-dev openssl libpq-dev libreadline6-dev libsqlite3-dev libssl-dev locate libsvn1 libtool libxml2 libxml2-dev libxslt-dev wget libyaml-dev ncurses-dev  postgresql-contrib xsel zlib1g zlib1g-dev -y
-    sudo rm -r msfinstall
-    clear
-    curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
-    sudo chmod 755 msfinstall
-    sudo ./msfinstall
-    sudo rm -rf msfinstall
-    clear
-    echo "INSTALLING STEGHIDE"
-    sudo apt install steghide -y
-    clear
-    echo "INSTALLING QRENCODE"
-    sudo apt install qrencode -y
-    clear
-    echo "INSTALLING FTP"
-    sudo apt install vsftpd -y && sudo apt install ftp -y
-    sudo systemctl stop ssh
-    sudo systemctl disable ssh
-    sudo systemctl stop vsftpd
-    sudo systemctl disable vsftpd
-    clear
-    echo "INSTALLING ZBAR-TOOLS"
-    sudo apt install zbar-tools -y
-    clear
-
-# takes away case in-sensitive on rasbian
-    echo "CHANGING CASE SENSITIVITY"
-    sudo echo 'set completion-ignore-case On' >> /etc/inputrc
-    sudo echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
-    clear
-
-# this portion is for KALI
-elif [[ "$(cat /etc/os-release | grep ID=kali)" == 'ID=kali' ]]; then
-  # do stuff for Kali
-  echo "This is Kali!"
-    echo "INSTALLING UPDATES"
-    sudo apt update -y #&& sudo apt upgrade -y
-    clear
-    echo "INSTALLING TERMINATOR"
-    sudo apt install terminator -y
-    clear
-    echo "INSTALLING QRENCODE"
-    sudo apt install qrencode -y
-    clear
-    echo "INSTALLING ZBAR-TOOLS"
-    sudo apt install zbar-tools -y
-    clear
-    echo "INSTALLING FTP"
-    sudo apt install vsftpd -y && sudo apt install ftp -y
-    sudo systemctl stop ssh
-    sudo systemctl disable ssh
-    sudo systemctl stop vsftpd
-    sudo systemctl disable vsftpd
-    clear
-    echo "INSTALLING STEGHIDE"
-    sudo apt install steghide -y
-    clear
+# update and upgrade
+echo "updating and upgrading..."
+if command -v apt &> /dev/null; then
+    sudo apt update -y && sudo apt upgrade -y
+elif command -v yum &> /dev/null; then
+    sudo yum update -y
 else
-  # Do something for other operating systems
-  echo "This is not Ubuntu, Raspbian or Kali."
-  # ...
+    echo "unsupported package manager. exiting."
+    exit 1
 fi
+clear
+
+# check for ubuntu
+if grep -q ID=ubuntu /etc/os-release; then
+    echo "this is ubuntu!"
+
+    # install ubuntu packages
+    install_packages terminator net-tools nmap arp-scan aircrack-ng wireshark tshark tree ettercap-text-only gpgv2 autoconf bison build-essential postgresql libaprutil1 libgmp3-dev libpcap-dev openssl libpq-dev libreadline6-dev libsqlite3-dev libssl-dev locate libsvn1 libtool libxml2 libxml2-dev libxslt-dev wget libyaml-dev ncurses-dev postgresql-contrib xsel zlib1g zlib1g-dev qrencode steghide vsftpd ftp zbar-tools
+
+    # install metasploit
+    sudo rm -r msfinstall
+    curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+    sudo chmod 755 msfinstall
+    sudo ./msfinstall
+    sudo rm -rf msfinstall
+
+    # additional ubuntu configs
+    echo 'set completion-ignore-case on' | sudo tee -a /etc/inputrc
+    clear
+
+# check for raspbian
+elif grep -q ID=raspbian /etc/os-release; then
+    echo "this is raspbian!"
+
+    # install raspbian packages
+    install_packages terminator net-tools nmap arp-scan ettercap-text-only aircrack-ng wireshark tshark tree qrencode steghide vsftpd ftp zbar-tools
+
+    # additional raspbian configs
+    echo 'set completion-ignore-case on' | sudo tee -a /etc/inputrc
+    clear
+
+# check for kali
+elif grep -q ID=kali /etc/os-release; then
+    echo "this is kali!"
+
+    # install kali packages
+    install_packages terminator qrencode zbar-tools vsftpd ftp steghide
+
+    # additional kali configs
+    clear
+
+# check for centos
+elif grep -q ID=centos /etc/os-release; then
+    echo "this is centos!"
+
+    # install centos packages
+    install_packages terminator net-tools nmap arp-scan aircrack-ng wireshark tshark tree ettercap gpgv2 autoconf bison make postgresql postgresql-server postgresql-contrib libpcap libpcap-devel openssl openssl-devel readline readline-devel sqlite sqlite-devel libyaml libyaml-devel libxml2 libxml2-devel libxslt libxslt-devel wget ncurses ncurses-devel zlib zlib-devel qrencode vsftpd ftp zbar-tools steghide
+
+    # additional centos configs
+    clear
+
+# if not
+else
+    echo "this is not ubuntu, raspbian, kali, or centos. exiting."
+    exit 1
+fi
+
+echo "script completed successfully."
