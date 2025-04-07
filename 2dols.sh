7ce30d6fe06e12846c02ba18334bbd8662f1c46b
BwithE <144924113+BwithE@users.noreply.github.com>
2025-04-06 21:03:11 -0400

    Update 2dols.sh

--- 2dols.sh
+++ 2dols.sh
@@ -19,7 +19,7 @@ cat << "EOF"
 EOF
 
 # Unified tool list (close to Kali)
-tool_list="aircrack-ng arp-scan bloodhound build-essential chisel curl exploitdb ftp g++-mingw-w64 gobuster gpgv2 libaprutil1 libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev libxslt-dev libyaml-dev libreoffice libreoffice-gtk4 ligolo-ng locate mingw-w64 ncat net-tools ncurses-dev nmap obsidian openssl postgresql postgresql-contrib python3-venv qrencode rlwrap seclists shellter steghide terminator tshark tree vsftpd wget wireshark wine xsel zbar-tools zlib1g zlib1g-dev veil ettercap-text-only"
+tool_list="aircrack-ng arp-scan bloodhound build-essential chisel curl exploitdb ftp g++-mingw-w64 gobuster gpgv2 libaprutil1 libgmp3-dev libpcap-dev libpq-dev libreadline6-dev libsqlite3-dev libssl-dev libsvn1 libtool libxml2 libxml2-dev libxslt-dev libyaml-dev libreoffice libreoffice-gtk4 ligolo-ng locate mingw-w64 ncat net-tools ncurses-dev nmap obsidian openssl postgresql postgresql-contrib python3 python3-venv qrencode rlwrap seclists shellter steghide terminator tshark tree vsftpd wget wireshark wine xsel zbar-tools zlib1g zlib1g-dev veil ettercap-text-only"
 
 # Detect OS
 OS=$(uname -a | tr '[:upper:]' '[:lower:]')
@@ -28,6 +28,20 @@ OS=$(uname -a | tr '[:upper:]' '[:lower:]')
 install_packages() {
     apt update -y && apt upgrade -y
     apt install -y $tool_list
+
+    # makes a directory for non-packaged tools
+    mkdir /opt/tools
+    git clone https://github.com/bwithe/lstnr /opt/lstnr
+    git clone https://github.com/bwithe/uppy /opt/uppy
+
+# latest linpeas.sh and winPEASx64.exe
+    bash -c "wget \$(curl -s https://api.github.com/repos/peass-ng/PEASS-ng/releases/latest | grep browser_download_url | grep linpeas.sh | cut -d '\"' -f 4) -O /opt/tools/linpeas.sh"
+    chmod +x /opt/tools/linpeas.sh
+    bash -c "wget \$(curl -s https://api.github.com/repos/peass-ng/PEASS-ng/releases/latest | grep browser_download_url | grep winPEASx64.exe | cut -d '\"' -f 4) -O /opt/tools/winPEASx64.exe"
+
+# pspy64
+    wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64 -O /opt/tools/pspy64
+    chmod +x /opt/tools/pspy64
 
     # Only install Metasploit if NOT Kali
     if [[ $1 == "install_msf" ]]; then
